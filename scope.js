7a2cbe28636dd3178b603aeb974487b490266146
Vikash Kumawat <vikashjalandra05@gmail.com>
2023-11-09 21:12:13 +0530

    hoisting

--- scope.js
+++ scope.js
@@ -6,10 +6,55 @@ let a=300;
 if (true) {
     let a =10
     const b=2
-    console.log(a);
+    // console.log(a);
 }
 
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
+
+
+function one() {
+    const username='vikash';
+
+    function two() {
+        const website='youtube'
+        console.log(username)
+    }
+    two()
+    // console.log(website)
+}
+
+// one()
+
+if (true) {
+    const username='vikash'
+    if (username==='vikash') {
+        const website='youtube'
+        // console.log(username+website);
+    }
+    // console.log(website)
+}
+
+// console.log(username);
+
+
+//+++++++++++ intresting ++++++++++++//
+
+console.log(addOne(6))
+function addOne(num) {
+    return num+1
+}
+
+
+// Hoisting problem
+// addTwo(6) 
+const addTwo=function(num) {
+    return num+2
+}
+
+const test =()=>{
+    console.log('Hello World');
+}
+test()
