369358ea3e6d5ec6b93c96169263d8d3ce9357ec
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-10 17:09:30 +0530

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// console.log("02">1);
+
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// console.log(null<0);
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
