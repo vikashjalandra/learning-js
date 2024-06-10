a197e2484c8391343bd5306c8f1eb76d7485798b
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-10 15:11:03 +0530

    conversion

---
+++ conversion.js
@@ -0,0 +1,16 @@
+let score = "33gbf"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33dgf" => NaN
+// true => 1; false =>0
+
+
+let 
---
+++ datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello") // only run in browser
+
+console.log(9+9)
+
+console.log('Error')
+
+let name = 'vikash'
+let age = 19
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => "" ,''
+// boolean => true/false
+// null => standalone value ()
+// undefined => 
+// symbol => unique 
+
+// object
+
+console.log(typeof undefined) // Output = undefined 
+console.log(typeof null) // output = object
