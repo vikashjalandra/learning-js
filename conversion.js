c1ab99142fa2c9c7b60f8556c6ebcc4a024675ae
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-10 17:00:02 +0530

    con

--- conversion.js
+++ conversion.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score)
 let isLoggedIn ="v"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1=> true; 0 => false
 // "" => false
@@ -26,5 +26,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* Operations **************************************
+
+
+let value = 3
+let negValue = -value
+
+
+
+let str1 ="hello"
+let str2 ="world"
+
+let str3 =str1+str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" +2 +2);
+// console.log(1 +2 +"2");
+
+// console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+
