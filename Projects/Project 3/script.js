d47fe8d2fc33e9326e9610b7d9ff8b855391d3a4
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-23 19:34:23 +0530

    project4

---
+++ Projects/Project 3/script.js
@@ -0,0 +1,8 @@
+// document.getElementById('clock')
+const clock =document.querySelector('#clock')
+
+
+setInterval(() => {
+    let date = new Date()
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000);
