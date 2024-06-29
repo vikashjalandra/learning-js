4aab6be5ce9d4b546e5d8b42433160d2f4191096
Vikash Kumawat <75132736+vikashjalandra@users.noreply.github.com>
2024-06-29 12:15:33 +0530

    c

---
+++ OOPs/getterSetter.js
@@ -0,0 +1,26 @@
+class User{
+    constructor(email,password){
+        this.email=email
+        this.password=password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+        this._email=value
+    }
+
+    get password(){
+        return `${this._password}hitesh`
+    } 
+
+    set password(value){
+        this._password=value
+    }
+}
+
+const vikash = new User('vikash@g.ai','abc')
+console.log(vikash.email);
+
